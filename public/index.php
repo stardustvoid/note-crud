b6913c36edfcbfed8cdee617938638be21ff5775
stardustvoid <112036089+stardustvoid@users.noreply.github.com>
2026-06-26 03:00:09 +0700

    Install Composer

--- public/index.php
+++ public/index.php
@@ -7,13 +7,9 @@ session_start();
 
 const BASE_PATH = __DIR__ . '/../';
 
+require BASE_PATH . 'vendor/autoload.php';
+
 require BASE_PATH . 'Core/functions.php';
-
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', '/', $class);
-
-    require base_path("{$class}.php");
-});
 
 require base_path('bootstrap.php');
 
